8b4c80fa7a69c06de565e20d10b4a275fdebad9b
IGdNara <116192560+Npswara@users.noreply.github.com>
2024-05-19 11:07:52 +0800

    Update sketch_oct20a.ino

--- sketch_oct20a.ino
+++ sketch_oct20a.ino
@@ -2,7 +2,7 @@ char inChar;
 char Char;
 #include <SoftwareSerial.h>
 
-SoftwareSerial SerialBT(2,3); // TX, RX
+SoftwareSerial SerialBT(2, 3); // TX, RX
 const int trigPin = 7;
 const int echoPin = 6;
 
@@ -15,45 +15,37 @@ void setup() {
 
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
-  pinMode(13,OUTPUT);
-  digitalWrite(13,LOW);
+  pinMode(13, OUTPUT);
+  digitalWrite(13, LOW);
 }
 
-void loop() 
-{ 
-  long duration, distance; // define variables
-  // generate the 10us pulse as the trigger signal according to the data sheet
+void loop() {
+  long duration, distance;
+
   digitalWrite(trigPin, LOW); 
   delayMicroseconds(2);
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
 
-  // duration is the time in us from sending the signal to receiving the echo
   duration = pulseIn(echoPin, HIGH);
-  
-  // distance is the distance in cm from the object to the sensor
-  // use the equation from the data sheet: distance(cm) = duration(us)/58
-  distance = duration/58;
-  
-  
-  if (SerialBT.available()) 
-  {
+  distance = duration / 58;
+
+  if (SerialBT.available()) {
     inChar = SerialBT.read();
     Serial.write(inChar);
   }
-  if(inChar == 'Y')
-  {
-    digitalWrite(13,HIGH);
+  
+  if (inChar == 'Y') {
+    digitalWrite(13, HIGH);
   }
- if (distance < 10) 
-  {
-    digitalWrite(13,LOW); 
-    }  
-  else if (inChar == 'N')
-  {
-    digitalWrite(13,LOW);
-  } 
-    Serial.print(distance); // print to IDE serial monitor
-    Serial.println(" cm");
+  
+  if (distance < 10) {
+    digitalWrite(13, LOW); 
+  } else if (inChar == 'N') {
+    digitalWrite(13, LOW);
+  }
+  
+  Serial.print(distance);
+  Serial.println(" cm");
 }
